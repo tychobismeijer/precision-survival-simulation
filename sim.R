75affd247410708e2b8f5c0eed60e6f54975ffd3
Tycho Bismeijer <t.bismeijer@nki.nl>
2021-10-03 21:04:50 +0200

    Add numbers from real data

--- sim.R
+++ sim.R
@@ -1,21 +1,41 @@
 library(dplyr)
+library(ggplot2)
 library(MatchIt)
 requireNamespace('multipleNCC')
+library(purrr)
 library(survival)
 
 set.seed(428361)
 
-n_experiments = 10
-
-n_high_risk <- 500
-n_low_risk <- 500
+n_experiments = 100
+risk_group_proportion = 0.5
 x_diff_mean <- 1.0
-x_sd <- 1.0
-lambda0 <- 1.0
-hazard_ratio <- 2.0
+x_sd <- 0.5
+lambda0 <- 0.001175099 # per month
+hazard_ratio <- 5.0
 
-n <- n_low_risk + n_high_risk
+months_per_year=12
+fu_up_to = 2011
 
+# From NKR
+incidence <- tribble(
+    ~year, ~n,
+    1989,  325,
+    1990,  338,
+    1991,  496,
+    1992,  632,
+    1993,  700,
+    1994,  757,
+    1995,  818,
+    1996,  792,
+    1997,  927,
+    1998,  980,
+    1999, 1020,
+    2000, 1104,
+    2001, 1120,
+    2002, 1038,
+    2003, 1142,
+    2004, 1300)
 
 methods <- list(
     cohort = function(subcohort, cohort, subcohort_idx) {
@@ -29,7 +49,7 @@ methods <- list(
             subcohort$event == 0,
             sum(cohort$event == 0) / sum(subcohort$event == 0),
             sum(cohort$event == 1) / sum(subcohort$event == 1))
-        coxph(Surv(time, event) ~ x, subcohort, weights=w)
+        coxph(Surv(time, event) ~ x, subcohort, weights=1/w)
     },
     kaplan_meier = function(subcohort, cohort, subcohort_idx) {
         cohort$sampling_p <- multipleNCC::KMprob(
@@ -42,35 +62,7 @@ methods <- list(
         subcohort$w <- cohort$sampling_p[subcohort_idx]
         subcohort <- subcohort[subcohort$w > 0,]
         coxph(Surv(time, event) ~ x, subcohort, weights=w)
-    },
-    km_exact = function(subcohort, cohort, subcohort_idx) {
-        Sur <- summary(survfit(Surv(time, event) ~ 1, cohort),
-                       times=subcohort$time)$surv
-        devKM <- function(w) {
-            subcohort$w <- w / sum(w)
-            SurW <- summary(survfit(Surv(time, event) ~ 1, subcohort, weights = subcohort$w),
-                            times = subcohort$time)$surv
-            mean((SurW-Sur)^2)
-        }
-        w <- ifelse(
-            subcohort$event,
-            sum(cohort$event) / sum(subcohort$event),
-            sum(1-cohort$event) / sum(1-subcohort$event))
-        w <- w / sum(w)
-        res <- optim(
-            par=w,
-            fn=devKM,
-            gr = NULL,
-            method = c("Nelder-Mead", "BFGS", "CG", "L-BFGS-B", "SANN","Brent")[4],
-            lower = 0,
-            upper = 1,
-            control = list(trace=3),
-            hessian = FALSE)
-        subcohort$w <- res$par / sum(res$par)
-        subcohort <- subcohort[subcohort$w > 0,]
-        coxph(Surv(time, event) ~ x, subcohort, weights=w)
     }
-
 )
 
 
@@ -83,22 +75,25 @@ for (i in 1:n_experiments) {
     message("Experiment ", i)
 
     # Cohort
-    cohort <- tibble(.rows=n)
-    cohort$risk_cat <- c(rep(0, n_low_risk), rep(1, n_high_risk))
-    cohort$x <- rnorm(n, x_diff_mean*cohort$risk_cat, x_sd)
-    cohort$lambda <- lambda0 * exp(log(hazard_ratio) * cohort$risk_cat)
-
-    cohort$time_to_event <- rexp(n, cohort$lambda)
-    cohort$follow_up_time <- runif(n, .1, .3)
+    cohort <- pmap_dfr(incidence, function(year, n) {
+        n_high_risk = floor(n * risk_group_proportion)
+        n_low_risk = n - n_high_risk
+        tibble(
+            follow_up_time = (fu_up_to - year - runif(n)) * months_per_year,
+            year=year,
+            risk_cat = rep(c(0, 1), times=c(n_low_risk, n_high_risk)) 
+    )})
+    cohort$x <- rnorm(nrow(cohort), x_diff_mean*cohort$risk_cat, x_sd)
+    cohort$lambda <- lambda0 * exp(log(hazard_ratio) * scale(cohort$risk_cat, scale=F))
+    cohort$time_to_event <- rexp(nrow(cohort), cohort$lambda)
     cohort$time <- pmin(cohort$follow_up_time, cohort$time_to_event)
     cohort$event <- cohort$follow_up_time >= cohort$time_to_event
 
     # Case-control
-    cohort$rounded_follow_up_time <- round(cohort$follow_up_time, 2)
     matching <- matchit(
-        event ~ rounded_follow_up_time,
+        event ~ year,
         data=cohort,
-        exact='rounded_follow_up_time',
+        exact='year',
         ratio=1)
     subcohort_idx <- which(!is.na(matching$subclass))
     subcohort <- cohort[subcohort_idx, ]
@@ -106,9 +101,6 @@ for (i in 1:n_experiments) {
     cohort$sampled[subcohort_idx] <- T
 
     print(table(cohort$event))
-    print(table(subcohort$event))
-
-    saveRDS(cohort, paste0('experiment_', i, '.Rds'))
 
     # Compute coefficients with different methods
     for (method in names(methods)) {
@@ -119,7 +111,21 @@ for (i in 1:n_experiments) {
 
 pdf('coeff_estimates.pdf')
 boxplot(coeff)
-abline(h=1.0, lty=2)
+abline(h=0.0, lty=2)
 boxplot(lapply(coeff, exp))
-abline(h=exp(1.0), lty=2)
+abline(h=1.0, lty=2)
+map_dfr(coeff, ~ tibble(coeff=.), .id='model') %>%
+    group_by(model) %>%
+    summarize(sd=sd(coeff), coeff=mean(coeff), se=sd/n(), lower=coeff-se, upper=coeff+se) %>%
+    ggplot(aes(x=model, y=coeff, ymin=lower, ymax=upper)) +
+    geom_point() +
+    geom_errorbar()
+dev.off()
+
+pdf('summary.pdf')
+boxplot(cohort$x ~ cohort$risk_cat)
+boxplot(cohort$x ~ cohort$event)
+survfit(Surv(time, event) ~ 1, cohort) %>% plot()
+survfit(Surv(time, event) ~ risk_cat, cohort) %>% plot()
+survfit(Surv(time, event) ~ x>0, cohort) %>% plot()
 dev.off()
